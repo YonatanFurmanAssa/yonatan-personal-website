8d72bd30a29a4de266359dd88111d96ae275e5e3
YonatanFurmanAssa <james8999123456789@gmail.com>
2026-03-17 05:50:18 +0200

    added some css tailwind fix

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class', // or 'selector' in newer v3 versions
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    // ...
+}
